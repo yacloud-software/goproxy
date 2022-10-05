cc6db93b092c25168fdda3f865ab40cc7a90f665
Conrad Wood <cnw@conradwood.net>
2022-10-06 00:47:43 +0100

    .

---
+++ src/golang.conradwood.net/goproxy/server/getpath.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	pb "golang.conradwood.net/apis/goproxy"
+	h2g "golang.conradwood.net/apis/h2gproxy"
+)
+
+type getpath_streamer struct {
+	ctx  context.Context
+	srv  pb.GoProxy_GetPathServer
+	size int
+}
+
+func (e *echoServer) GetPath(req *pb.GetPathRequest, srv pb.GoProxy_GetPathServer) error {
+	sr := &h2g.StreamRequest{Path: req.Path}
+	sv := &getpath_streamer{ctx: srv.Context(), srv: srv}
+	err := e.streamHTTP(sr, sv)
+	fmt.Printf("Sent %d bytes for path \"%s\"\n", sv.size, req.Path)
+	return err
+
+}
+
+func (gs *getpath_streamer) Send(d *h2g.StreamDataResponse) error {
+	bd := &pb.BinData{Data: d.Data}
+	gs.size = gs.size + len(bd.Data)
+	err := gs.srv.Send(bd)
+	return err
+}
+func (gs *getpath_streamer) Context() context.Context {
+	return gs.ctx
+}
